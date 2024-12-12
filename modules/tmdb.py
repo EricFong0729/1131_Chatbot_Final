9e9d8cbdd11c11f6435168d39b66b07e3e7ed8c7
EricFong0729 <130958929+EricFong0729@users.noreply.github.com>
2024-12-12 10:12:23 +0800

    Update tmdb.py

--- modules/tmdb.py
+++ modules/tmdb.py
@@ -8,11 +8,13 @@ config.read('config.ini')
 # 獲取 API key
 api_key = config['tmdb']['api_key']
 
-url = "https://api.themoviedb.org/3/authentication"
+# 取得電影名稱
+query=""
+url = "https://api.themoviedb.org/3/search/movie?query="++"&include_adult=false&language=en-US&page=1"
 
 headers = {
     "accept": "application/json",
-    "Authorization": f"Bearer {api_key}"
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNDAyOGQwMWE3YTgzYjZjZmIyZTQ1OGRkOWU2YjNmNCIsIm5iZiI6MTczMzg4NTc1MS4xMjQsInN1YiI6IjY3NThmZjM3MzU5ZGM1ZmM4ODNmMGRkYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8q7p7jgB2m46go5a0KBV1uc3sLeflCmsGWjfb26R1hA"
 }
 
 response = requests.get(url, headers=headers)
